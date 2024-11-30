2575188c6d693b7eb2f2754a4ee2c8e968140256
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-30 20:19:22 +0530

    list exercise

---
+++ Python Fundamentals/Exercise Programs/Exercise2.py
@@ -0,0 +1,14 @@
+numbers=[]
+strings=[]
+names=["John","Eric","Jessica"]
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings.append("Hello")
+strings.append("World")
+secondName=names[1]
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % secondName)
