f1026801f67e727c02ac0e5b28cd77d7c4866b94
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-01 00:31:09 +0530

    boolean operators

---
+++ Python Fundamentals/Conditionals/BooleanOperator.py
@@ -0,0 +1,8 @@
+name="Divyansh"
+age=20
+
+if name=="Divyansh" and age==20:
+    print("Your name is Divyansh, and your are 20 years old")
+
+if name=="Divyansh" or name=="Sumit":
+    print("You are either Divyansh or Sumit")
