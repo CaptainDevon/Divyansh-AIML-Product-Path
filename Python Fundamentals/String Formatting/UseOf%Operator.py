01d256f3f9c3e345ab5d89a1a5ce126a24a22f38
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-01 00:00:25 +0530

    string formatting

--- Python Fundamentals/String Formatting/UseOf%Operator.py
+++ Python Fundamentals/String Formatting/UseOf%Operator.py
@@ -2,5 +2,3 @@ name="Divyansh"
 surName="Sinha"
 age=20
 print("Hello, %s %s! and Your age is %d"%(name,surName,age))
-# we can also write
-print("Hello, {} {}! and Your age is {}".format(name,surName,age))
---
+++ Python Fundamentals/String Formatting/WithoutUsing%Operator.py
@@ -0,0 +1,4 @@
+name="Divyansh "
+surName="Sinha"
+age=20
+print("Hello, {} {}! and Your age is {}".format(name,surName,age))
