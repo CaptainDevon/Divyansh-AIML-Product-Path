5c113fb2745201afb2cc4e1f878321c218a8eddc
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-01 00:33:43 +0530

    In Operator

---
+++ Python Fundamentals/Conditionals/InOperator.py
@@ -0,0 +1,7 @@
+name="Divyansh"
+name2="Rage"
+if name in ["Divyansh","Sumit","Bhawna"]:
+    print("You are the family member")
+
+if name in ["Divyansh","Sumit","Bhawna"]:
+    print("You are an outsider")
